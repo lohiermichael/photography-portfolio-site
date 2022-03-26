2d9b342e2538e69b9710906ed095984ff9375437
Michael Lohier <lohiermichael@gmail.com>
2022-03-26 20:48:27 +0100

    Adapt to phone layouts

--- my_version/public/js/script.js
+++ my_version/public/js/script.js
@@ -1,12 +1,29 @@
+const header =  document.querySelector('.header');
+const nav = document.querySelector('.nav');
 const navButton = document.querySelector('.nav__button');
+const menuNav = document.querySelector('.menu-nav')
+const burger = document.querySelector('.burger')
+const logo =  document.querySelector('.logo');
+const footer =  document.querySelector('.footer');
+
+const elementsToChangeOpen = [
+    nav,
+    navButton,
+    header,
+    burger,
+    menuNav,
+    logo,
+    footer
+]
+
 let isMenuOpen = false
 
 navButton.addEventListener('click', () => {
     if  (isMenuOpen) {
-        navButton.classList.remove('menu-open');
+        elementsToChangeOpen.map(element => element.classList.remove('menu-open'))
         isMenuOpen = false;
     } else {
-        navButton.classList.add('menu-open');
+        elementsToChangeOpen.map(element => element.classList.add('menu-open'))
         isMenuOpen = true;
     }
 })
