9114e446d9a87cdc4e5a3033f969469099d94223
Michael Lohier <lohiermichael@gmail.com>
2022-10-29 20:11:59 +0200

    Close mobile menu on resize
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- my_version/src/public/js/menu-open.js
+++ my_version/src/public/js/menu-open.js
@@ -4,9 +4,17 @@ import { elementsToChangeOpen } from './header.js'
 
 const navButton = document.querySelector('.nav__button');
 
-navButton.addEventListener('click', () => {
+function toggleMobileMenu() {
     elementsToChangeOpen.map(element => {
         element.classList.toggle('menu-open');
     })
-});
+}
 
+navButton.addEventListener('click', () => toggleMobileMenu());
+
+// Close mobile menu on resize
+window.addEventListener('resize', () => {
+    const isMenuOpened = elementsToChangeOpen[0].classList.contains('menu-open');
+    if (window.innerWidth >= 768 & isMenuOpened) toggleMobileMenu();
+
+});
