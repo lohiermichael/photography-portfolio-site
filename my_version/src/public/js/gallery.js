610c4066acdc3fe0b36dda36b0c365c66a254503
Michael Lohier <lohiermichael@gmail.com>
2022-10-29 23:54:40 +0200

    Remove the iffy initial reveal logic

--- my_version/src/public/js/gallery.js
+++ my_version/src/public/js/gallery.js
@@ -17,44 +17,19 @@ galleryContainer.addEventListener('wheel', (scrollingEvent) => {
 
 const galleryImages = galleryContainer.querySelectorAll('img');
 
-// Only reveal visible images at launched
-
-// Launch animation properties
-let name = 'revealAnimation';
-let duration = '1s';
-let fillMode = 'forwards';
-
-let initialRevealedIndex = 1;
-const ANIMATION_INIT_TIME = 1; // seconds
-const ANIMATION_MULTIPLIER = 0.3;
-
-galleryImages.forEach(image => {
-  image.addEventListener('load', () => {
-    if (isImageVisible(image)) {
-      let delay = String(
-        ANIMATION_INIT_TIME + ANIMATION_MULTIPLIER * initialRevealedIndex
-      ) + 's';
-      let animation = `${duration} ${delay} ${fillMode} ${name}`
-      initialRevealedIndex += 1
-      image.style.animation = animation;
-
-    }
-  })
-})
-
 // When scrolling revealing images that appear
 galleryContainer.addEventListener('wheel', () => {
   galleryImages.forEach(image => {
     const imageVisible = isImageVisible(image);
     if (!(image.classList.contains('reveal')) & imageVisible) {
+      console.log('here');
       if (image.style.animationName == 'revealAnimation') {
-        console.log('object');
         image.style.removeProperty('animation')
       }
-      image.classList.replace('hide', 'reveal');
+      image.classList.add('reveal');
     }
     else if ((image.classList.contains('reveal')) & !imageVisible) {
-      image.classList.replace('reveal', 'hide');
+      image.classList.remove('reveal');
     }
   })
 });
@@ -62,9 +37,12 @@ galleryContainer.addEventListener('wheel', () => {
 function isImageVisible(image) {
   const imageBounding = image.getBoundingClientRect();
   return (
-      imageBounding.top < window.outerHeight &
+      imageBounding.top < window.innerHeight &
       imageBounding.bottom > 0 &
       imageBounding.right > 0 &
-      imageBounding.left < window.outerWidth
+      imageBounding.left < window.innerWidth
     );
 }
+
+// Prevent right click for downloading images
+document.addEventListener('contextmenu', event => event.preventDefault());
