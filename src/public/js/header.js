7d07cac796dccbb3d9f1c38451ee97090ffbed84
Michael Lohier <lohiermichael@gmail.com>
2022-12-03 17:11:13 +0100

    Redesign the menu for mobiles
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -1,9 +1,9 @@
 export const elementsToChangeOpen = [
-    document.querySelector('.header'),
-    document.querySelector('.nav'),
-    document.querySelector('.nav__button'),
+    // document.querySelector('.header'),
+    // document.querySelector('.nav'),
+    // document.querySelector('.nav__button'),
     document.querySelector('.menu-nav'),
     document.querySelector('.burger'),
     document.querySelector('.logo'),
-    document.querySelector('.footer'),
+    // document.querySelector('.footer'),
 ];
