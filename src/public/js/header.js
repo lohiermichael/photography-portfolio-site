3484416e02ad9df9bcb8e041e25df2c495aa6d2b
Michael Lohier <lohiermichael@gmail.com>
2022-12-03 22:04:18 +0100

    Add footer to the redesign
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -5,5 +5,5 @@ export const elementsToChangeOpen = [
     document.querySelector('.menu-nav'),
     document.querySelector('.burger'),
     document.querySelector('.logo'),
-    // document.querySelector('.footer'),
+    document.querySelector('.footer'),
 ];
