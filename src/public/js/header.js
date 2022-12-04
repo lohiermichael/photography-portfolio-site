cf1316eaf28f67017614968ddd6247af91a48106
Michael Lohier <lohiermichael@gmail.com>
2022-12-04 22:40:46 +0100

    Remove unused JS
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- src/public/js/header.js
+++ src/public/js/header.js
@@ -1,7 +1,4 @@
 export const elementsToChangeOpen = [
-    // document.querySelector('.header'),
-    // document.querySelector('.nav'),
-    // document.querySelector('.nav__button'),
     document.querySelector('.menu-nav'),
     document.querySelector('.burger'),
     document.querySelector('.logo'),
